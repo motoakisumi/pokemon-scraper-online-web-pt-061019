818dd1e3ec8eb263094c40e672d9d11917b059d3
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-09-16 01:23:38 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,7 +2,7 @@ class Pokemon
   attr_accessor :name, :type, :db
   attr_reader :id
 
-  def self.initialize(id = nil, name, type, db)
+  def initialize(id = nil, name, type, db)
     @name = name
     @type = type
     @id = id
