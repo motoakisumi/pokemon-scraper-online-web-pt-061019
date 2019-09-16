727ac1396c0ce5a9bf41d1330eef866083ffaff6
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-09-16 01:38:24 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -13,9 +13,9 @@ class Pokemon
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?);", name, type)
   end
 
-  def self.find(id)
+  def self.find(id, db)
     result = df.execute("SELECT * FROM pokemon WHERE id = ?", id)[0]
-    Pokemon.new(result[0], result[1], result[2], result[3])
+    Pokemon.new(result[0], result[1], result[2], db)
   end
 
 end
